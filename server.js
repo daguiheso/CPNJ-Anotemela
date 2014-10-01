e56799ae8384fda1cc46244d635ebcb04b445eb3
Daniel <danielhernandez16@hotmail.com>
2014-10-01 11:06:30 -0700

    Paso 5:Modularizando el recurso notas

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var bodyParser = require('body-parser');
 * Local Variables
 */
 var server = express();
-var db = {};
+
 /**
 * Middleware
 */
@@ -19,32 +19,8 @@ server.use(bodyParser.json('application/json'));
 /**
 * Routes
 */
-server.post('/notas', function(req, res){
-	console.log('POST', req.body.nota);
-	var notaNueva = req.body.nota;
-	notaNueva.id = Date.now();//Date.now retorna una fecha en cadena, esto para asegurarnos que sea unico el id
-	
-	db[notaNueva.id] = notaNueva;
-
-	res
-		.status(201)
-		.json({
-			nota: notaNueva
-		});
-});
-
-
-server.get('/notas/:id', function(req, res){
-	console.log('GET /notas/%s', req.params.id); // ver propiedades de request en Doc de express
-	var id = req.params.id; // guardamos id
-	var nota = db[id]; // buscando la nota
-	
-	res		
-		.json({
-			notas: nota
-		});
-});
-
+var notas = (require('./lib/notas'));
+server.use(notas);
 /**
 * Expose or star server
 */
