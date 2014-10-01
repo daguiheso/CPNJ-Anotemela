93e481e1dee61445338aa8c2f5e13da690c6a586
Daniel <danielhernandez16@hotmail.com>
2014-10-01 10:06:21 -0700

    Paso 3:Escribiendo las pruebas de GET para una nota

--- test/notas.js
+++ test/notas.js
@@ -38,8 +38,49 @@ describe('recurso /notas', function(){
 					expect(nota).to.have.property('body', 'soy el cuerpo de json'); 
 					expect(nota).to.have.property('id'); //esperamos que tenga un id
 					done();
-
 				});
 		});
+	});
+
+	describe('GET', function(){
+		it('deberia obtener una nota existente', function(done){
+			var data =
+			{
+				"nota":{
+					"title": "Mejorando.la #node-pro",
+					"description": "Introduccion a clase",
+					"type": "js",
+					"body": "soy el cuerpo de json"
+				}
+			};
+
+			// Crear nota nueva		
+			request
+				.post('/notas')
+				.set('Accept', 'application/json')
+				.send(data)			
+				.expect(201)
+				.expect('Content-Type', /application\/json/)
+
+				//despues de evaluar que se creo, evaluaremos que exista
+				.end(function(err, res){  //cundo se crea nota, aqui responden con una solicitud
+					var id = res.body.nota.id; //guardamos el id porque vamos a uerer solicitar la misma nota que acabamos de crear
+
+					request
+						.get('/notas/' + id)
+						.expect(200)
+						.expect('Content-Type', /application\/json/)
+						.end(function(err, res){
+							var nota = res.body.notas;
+							expect(nota).to.have.property('title', 'Mejorando.la #node-pro');
+							expect(nota).to.have.property('description', 'Introduccion a clase');
+							expect(nota).to.have.property('type', 'js');
+							expect(nota).to.have.property('body', 'soy el cuerpo de json');
+							//esperamos que el id sea igual al id de la nota que creamos anteriormente
+							expect(nota).to.have.property('id', id);  
+							done();																							
+						});
+				});	
+			});
 	});
 });
