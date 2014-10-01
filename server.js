eb5d45c6c58963dbd2f2a051dd82723af1d406bd
Daniel <danielhernandez16@hotmail.com>
2014-10-01 09:31:45 -0700

    Paso 2:Creando la primera ruta y testing

--- server.js
+++ server.js
@@ -1,15 +1,48 @@
+// para poder tener acceso a la informacion que nos envia un cliente en express, usamos un modulo que server
+// llama body-parser, nos ayuda a evaluar el cuerpo de una solicitud y nos extrae la informacion
+
+/**
+* Dependencies
+*/
 var express = require('express');
+var bodyParser = require('body-parser');
 
-var app = express();
-
-
-
-app.listen(3000, function(){
+/**
+* Local Variables
+*/
+var server = express();
+/**
+* Middleware
+*/
+server.use(bodyParser.json('application/json'));
+/**
+* Routes
+*/
+server.post('/notas', function(req, res){
+	console.log('POST', req.body.nota);
+	var notaNueva = req.body.nota;
+	notaNueva.id = 123;
 	
-		console.log('hola anotamela desde http://localhost:3000');
+	res
+		.status(201)
+		.json({
+			nota: notaNueva
+		});
+});
 
-		})
+/**
+* Expose or star server
+*/
+
+// si queremos saber si alguien nos esta usando a nosotros como un modulo o si somos 
+// el modulo que esta siendo usado
+
+if (!module.parent) { // si tengo a un padre es porque me estan usando como modulo
+	server.listen(3000, function(){
+		console.log('hola estoy escuchando en el url http: ....3000');
+	});
+}else{
+	module.exports = server;
+}
 
 
-
-		module.exports = app;
--- test/notas.js
+++ test/notas.js
@@ -6,7 +6,7 @@ request = request(host); //vamos a iniciar nuestra libreria con el servidor
 
 describe('recurso /notas', function(){
 	describe('POST', function(){
-		it('debera crear una nota nueva', function(){
+		it('debera crear una nota nueva', function(done){
 			// throw new Error('tengo hambre');
 			// return true;
 			var data = {
@@ -26,16 +26,18 @@ describe('recurso /notas', function(){
 				.expect('Content-Type', /application\/json/)  
 				//hasta aqui ya hicimos la solicitud
 				.end(function(err, res){   // pasandole un callback a nuestro request
+
 					//examinando nuestra nota
 					var body = res.body; // el cuerpo de ls respuesta, lo llamamos cuerpo
 					expect(body).to.have.property('nota'); // esperamos que el halla una propiedad nota
 					var nota = body.nota;
+
 					expect(nota).to.have.property('title', 'Mejorando.la #node-pro'); 
 					expect(nota).to.have.property('description', 'Introduccion a clase'); 
 					expect(nota).to.have.property('type', 'js'); 
 					expect(nota).to.have.property('body', 'soy el cuerpo de json'); 
 					expect(nota).to.have.property('id'); //esperamos que tenga un id
-
+					done();
 
 				});
 		});
